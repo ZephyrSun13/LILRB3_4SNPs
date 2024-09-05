3f56d1446d418a27932c32cc710b30b66d36e3ab
ZephyrSun13 <35071472+ZephyrSun13@users.noreply.github.com>
2024-09-05 12:03:21 -0400

    Delete 5.Figures directory

--- 5.Figures/Fig2/fig2b.plotEnrichNP.sh
+++
@@ -1,5 +0,0 @@
-
-ml R/4.0.3
-
-Rscript fig2b.plotEnrichNP.r DCGL.over.xls.SigSNPTab.sele.xls.ann.xls.Gene_EnrichR.sig.xls DCGL.over.xls.SigSNPTab.sele.xls.ann.xls.Gene_EnrichR.sig.xls.enrich.plot "Green;Green"
-
